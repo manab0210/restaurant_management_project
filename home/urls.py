f2a09381cf65436d077743752fa19cf2d79d9735
Manab Bera <manabbera2005@gmail.com>
2026-03-20 10:16:52 +0530

    67775

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from .views import MenuCategoryListView
 
 urlpatterns = [
-    
+    path('api/categories',MenuCategoryListView.as_view(),name='category_list')
 ]
