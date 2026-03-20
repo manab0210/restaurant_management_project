46a58943b05e3784bc4ee6166d21319f01fa5bdf
Manab Bera <manabbera2005@gmail.com>
2026-03-20 10:34:40 +0530

    67774

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
+
+class Coupon(models.Model):
+    code=models.CharField(max_lenght=50,unique=True)
+    discount_percentage=models.DecimalField(
+        max_digits=3,
+        decima;_places=2,
+        validators=[MinValueValidator(0),MaxValueValidator(1)]
+    )
+    is_active=models.BooleanField(default=True)
+    valid_form=models.DateField()
+    valid_until=models.DateField()
+    
+    def __str__(self):
+        return f"{self.code}({self.discount_percentage*100}%)"
