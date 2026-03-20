6011986d43a1504574399f8edc62e7fdb5d8e2dc
Manab Bera <manabbera2005@gmail.com>
2026-03-20 10:58:28 +0530

    67772

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 class OrderStatus(models.Model):
     name=models.CharField(max_lenght=50,unique=True)
+    status =models.FroeginKey(
+        'OrderStatus',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='orders'
+    )
     def __str__(self):
         return self.name
+        return f"Order #{self.id} - {self.status}"
     class Meta:
         verbose_name_plural="Order Statuses"
