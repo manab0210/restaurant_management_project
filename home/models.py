a0ef2a15c4ecce7ca0aa3579b38435c727c497c3
Manab Bera <manabbera2005@gmail.com>
2026-03-20 10:24:38 +0530

    67771

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import db,models
 
 # Create your models here.
+class MenuCategory(models.Model):
+    name=models.CharField(max_lenght=100, unique=True)
+
+    def __str__(self);
+    return self.name
+    
+    class Meta:
+        verbose_name_plural="MenuCategories"
