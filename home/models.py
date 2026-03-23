bf7a79bef2f26ba1fb2fa54407b6baf2f9a717c1
Manab Bera <manabbera2005@gmail.com>
2026-03-23 19:26:49 +0530

    67849

--- home/models.py
+++ home/models.py
@@ -2,10 +2,10 @@ from django.db import db,models
 
 # Create your models here.
 class MenuCategory(models.Model):
-    name=models.CharField(max_lenght=100, unique=True)
-
+    # name=models.CharField(max_lenght=100, unique=True)
+    is_featured = models.BooleanField(default=False)
     def __str__(self);
-    return self.name
+    return self.title
     
-    class Meta:
-        verbose_name_plural="MenuCategories"
+    # class Meta:
+    #     verbose_name_plural="MenuCategories"
