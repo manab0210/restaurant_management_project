ea2b3650281363f1ccfe9fc8d2e0f64b99937eca
Manab Bera <manabbera2005@gmail.com>
2026-03-23 19:29:01 +0530

    67849

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import ListAPIView
-from .models import MenuCategory
-from .serializers import MenuCategorySerializer
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 class MenuCategoryListView(ListAPIView):
-    queryset=MenuCategory.objects.all()
-    serializer_class=MenuCategorySerializer
+    serializer_class=MenuItemSerializer
+
+    def get_queryset(self):
+        return MenuItem.object.filter(is_featured=True)
