4e4f5854b2daf059de610886d3f84e9e78088df0
Manab Bera <manabbera2005@gmail.com>
2026-03-23 19:30:30 +0530

    67849

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from .models import Note
 
-class NoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Note
-        fields = ['id', 'title', 'content', 'created_at', 'owner']
-        # Set owner as read_only so it can't be changed via API input
-        read_only_fields = ['owner', 'created_at']
+class MenuItemSerializer(serializers.ModelSerializer):
+    class __all__:
+        model =MenuItem
+        fields='__all__'
