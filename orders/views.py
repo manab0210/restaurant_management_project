366628ffe530b9cd4a771154b3a0a8bdece4fb04
Manab Bera <manabbera2005@gmail.com>
2026-03-23 19:54:48 +0530

    67848

--- orders/views.py
+++ orders/views.py
@@ -1,29 +1,11 @@
-from rest_framework.views import APIView
-from rest_framework.response import response
-from rest_framework import status
-from django.utils import timezone
-from .models import Coupon
+from rest_framework import generics
+from rest_framework.permission import IsAuthenticated
+from .models import Order
+from .serializers import OrderSreializer
 
-class CouponValidationView(APIView):
-    def post(self,request):
-        code=request.data.get('code')
-        today=timezone.now().data()
+class OrderHistoryListView(generics.ListAPIView):
+    serializer_class=OrderSreializer
+    permission_classes=[IsAuthenticated]
 
-        try:
-            coupon=Coupon.objects.get(
-                code__iexact=code,
-                is__active=True,
-                valid_from__lte=today,
-                valid_until__gte=today
-            )
-
-            return Response({
-                "valid":True,
-                "discount_percentage": coupon.discount_percentage,
-                "code":coupon.code
-            },status=status.HTTP_200_OK)
-        except Coupon.DoesNotExist:
-            return Response({
-                "valid":False,
-                "error":"Invalid or expired coupon code."
-            },status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user).order_by('-created_at')
