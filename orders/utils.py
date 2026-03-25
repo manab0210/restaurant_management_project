f79a3ab0f8a41896c7e8f5f1fe60f0eb0ba12bb9
Manab Bera <manabbera2005@gmail.com>
2026-03-25 10:03:02 +0530

    68166

--- orders/utils.py
+++ orders/utils.py
@@ -1,12 +1,20 @@
-from django.utils import timezone
-from .modles import DailyOperatingHours
+# from django.utils import timezone
+# from .modles import DailyOperatingHours
 
-def get_today_operating_hours():
-    current_day=timezone.now().strftime('%A')
+# def get_today_operating_hours():
+#     current_day=timezone.now().strftime('%A')
 
-try:
-    hours_entry=DailyOperatingHours.objects.get(day_name=current_day)
+# try:
+#     hours_entry=DailyOperatingHours.objects.get(day_name=current_day)
 
-    return (hours_entry.open_time,hours_entry.close_time)
-except DailyOperatingHours.DoesNotExist:
-    return(None,None)
+#     return (hours_entry.open_time,hours_entry.close_time)
+# except DailyOperatingHours.DoesNotExist:
+#     return(None,None)
+from django.db.modles import Sum
+from .modles import Order
+
+def get_daily_sales_total(date):
+    """Calculates total sales for a specific date.
+    'date' should be a datetime.date object."""
+    daily_orders=Order.objects.filter(created_at__date=date)
+    result=daily_orders.aggregate(total_sum=Sum('total_price'))
