c2d3a3a68755978ae39de4745c07c02be7ec059e
manab0210 <manabbera2005@gmail.com>
2026-03-31 20:20:27 +0530

    open_closing

--- notes_app/views.py
+++ notes_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
+class NoteViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing note instances.
+    """
+    serializer_class = NoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # This ensures users only see their own notes
+        return Note.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically set the owner to the currently logged-in user
+        serializer.save(owner=self.request.user)
