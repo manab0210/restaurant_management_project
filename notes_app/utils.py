ef7f1412ab4b8238dc7a707e84df16cb86174a3d
Manab Bera <manabbera2005@gmail.com>
2026-04-01 15:42:27 +0530

    68953

--- notes_app/utils.py
+++ notes_app/utils.py
@@ -1,21 +1,27 @@
-from datetime import datetime
-def is_restaurant_open():
-    now=datetime.now()
-    current_day=now.strftime('%A')
-    current_time=now.time()
-    schedule={
-        'Monday':(9,0,22,0),
-        'Tuesday':(9,0,22,0),
-        'Wednesday':(9,0,22,0),
-        'Thursday':(9,0,22,0),
-        'Friday':(9,0,23,0),
-        'Saturday':(10,0,23,0),
-        'Sunday':(10,0,21,0),
-    }
-    start_h,start_m,end_h,end_m=schedule.get(current_day)
-    from datetime import time
-    opening_time=time(start_h,start_m)
-    closing_time=time(end_h,end_m)
-    if opening_time<=current_time<=closing_time:
-        return True
-    return False
+# from datetime import datetime
+# def is_restaurant_open():
+#     now=datetime.now()
+#     current_day=now.strftime('%A')
+#     current_time=now.time()
+#     schedule={
+#         'Monday':(9,0,22,0),
+#         'Tuesday':(9,0,22,0),
+#         'Wednesday':(9,0,22,0),
+#         'Thursday':(9,0,22,0),
+#         'Friday':(9,0,23,0),
+#         'Saturday':(10,0,23,0),
+#         'Sunday':(10,0,21,0),
+#     }
+#     start_h,start_m,end_h,end_m=schedule.get(current_day)
+#     from datetime import time
+#     opening_time=time(start_h,start_m)
+#     closing_time=time(end_h,end_m)
+#     if opening_time<=current_time<=closing_time:
+#         return True
+#     return False
+
+import re def is_valid_phone_number(phone_string):
+phone_regex=r'^(\+?\d{1,3})?[ \.-]?\(?\d{3}\)?[ \.-]?\d{3}[ \.-]\d{4}$'
+if re.match(phone_regex,phone_string):
+    return True
+return False
