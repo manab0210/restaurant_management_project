10591f3dfe6821195c2ae0284c9715d5169f25ba
Manab Bera <manabbera2005@gmail.com>
2026-04-01 15:17:32 +0530

    68952

--- home/views.py
+++ home/views.py
@@ -1,17 +1,6 @@
-# from rest_framework.generics import ListAPIView
-# from .models import MenuItem
-# from .serializers import MenuItemSerializer
-# class MenuCategoryListView(ListAPIView):
-#     serializer_class=MenuItemSerializer
-
-#     def get_queryset(self):
-#         return MenuItem.object.filter(is_featured=True)
-from django.shortcuts import render
-from .models import DailySpecial
-
-def home(request):
-    context={
-        'featured_special':DailySpecial.get_random_special()
-        
-    }
-    return render(request,'home/index.html',context)
+from rest_framework.generics import ListAPIView
+from .models import MenuItem
+from .serializers import MenuItemIngredientSerializer
+class MenuItemIngredientsView(RetrieveAPIView):
+    queryset=MenuItem.objects.all()
+    serializer_class=MenuItemIngredientsSerializer
