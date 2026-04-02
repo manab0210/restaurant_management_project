92c174e73cc74b419cce1e71cf9aa84d28b5bc82
Manab Bera <manabbera2005@gmail.com>
2026-04-02 21:13:54 +0530

    68955

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
-from .views import MenuItemIngredientsView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MenuItemViewSet
+
+router= DefaultRouter()
+router.register(r'menu-items',MenuItemViewSet)
+
 urlpatterns=[
-    path('api/menu-items/<int:pk>/ingredients/',MenuItemIngredientsView.as_view(),name='menu-item-ingredient')
+    path('',include(router.urls)),
 ]
