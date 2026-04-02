aaaeb0c082270e46a2a72462a62a552528b610db
Manab Bera <manabbera2005@gmail.com>
2026-04-02 20:57:11 +0530

    68954

--- home/models.py
+++ home/models.py
@@ -1,29 +1,16 @@
-# from django.db import db
-# from django.db import models
-
-# # Create your models here.
-# class MenuCategory(models.Model):
-#     # name=models.CharField(max_lenght=100, unique=True)
-#     is_featured = models.BooleanField(default=False)
-#     def __str__(self);
-#     return self.title
-    
-#     # class Meta:
-#     #     verbose_name_plural="MenuCategories"
-# class Restaurant(models.Model):
-#     has_delivery=models.BooleanField(default=False)
-#     def __clstr__(self):
-        # return self.name
-
-import random
+import datetime 
 from django.db import models
-
+class DailySpecialManager(models.Manager):
+    def upcoming(self):
+        today = datetime.date.today()
+        return self.filter(date__gte=today)
 class DailySpecial(models.Model):
-    name=models.CharField(max_lenght=100)
+    name=models.CharField(max_length=100)
     description = models.TextField()
-    price=models.DecimalField(max_digits=6,decimail_places=2)
-    is_active=models.BooleanField(default=True)
-    @staticmethod
-    def get_random_special():
-        random_special=DailySpecial.objects.filter(is_active=True).order_by('?').first()
-        returnrsndom_special
+    date= models.DateField()
+    price= models.DecimalField(max_digits=6,decimal_places=2)
+
+    objects=DailySpecialManager()
+
+    def __str__(self):
+        return f"{self.name}({self.date})"
