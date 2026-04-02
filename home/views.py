bace6847fb9a4649abf4b57b56df86e4be7b3871
Manab Bera <manabbera2005@gmail.com>
2026-04-02 21:10:55 +0530

    68955

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import viewsets, permissions
 from .models import MenuItem
-from .serializers import MenuItemIngredientSerializer
-class MenuItemIngredientsView(RetrieveAPIView):
+from .serializers import MenuItemSerializer
+class MenuItemViewSet(viewsets.ModelViewSet):
     queryset=MenuItem.objects.all()
-    serializer_class=MenuItemIngredientsSerializer
+    serializer_class=MenuItemSerializer
+
+    def get_permissions(self):
+        if self.action in ['update','partial_update','destroy','create']:
+            permission_classes = [permissions.IsAdminUser]
+        else:
+            permission_classes=[permissions.AllowAny]
+        return [permission() for permission in permission_classes]
