9dea77c8c3f9631aed8f777928b2c7275fbe8661
Manab Bera <manabbera2005@gmail.com>
2026-04-05 19:52:04 +0530

    69247

--- notes_app/models.py
+++ notes_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Note(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+class Category(models.Model):
+    name=models.CharField(max_length=255,unique=True)
+    def __str__(self):
+        return self.name
+class MenuItem(models.Model):
+    title=models.CharField(max_length=255)
+    price=models.DecimalField(max_digits=6,decimal_place=2)
+    category=models.ForeignKey(Category,on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
