d72268679eddd6a8f61a863f3f960a60a8d4d957
Manab Bera <manabbera2005@gmail.com>
2026-04-05 19:48:28 +0530

    69247

--- notes_app/serializers.py
+++ notes_app/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import MenuItem
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    category_name=serializers.ReadOnlyField(source='category.name')
     class Meta:
         model=MenuItem
-        fields=['id','name','description','price']
+        fields=['id','title','price','category','category_name']
