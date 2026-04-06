2a74921f1e31196a24fe7f29e8a38681fcebe0ca
Manab Bera <manabbera2005@gmail.com>
2026-04-06 19:23:01 +0530

    69250

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Order
+@admin.action(description="Mark selected orders as Processed")
+def mark_orders_processed(modeladmin,request,queryset):
+    wpdated_count=queryset.update(status='Processed')
+    modeladmin.message_user(request,f"Successfully marked {updated_count} orders as Processed.")
+class OrderAdmin(admin.ModelsAdmin):
+    list_display=['id','customer_name','status','created_at']
+    actions=[mark_orders_processed]
+admin.site.register(Order,OrderAdmin)
