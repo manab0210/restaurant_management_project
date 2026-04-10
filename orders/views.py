ab5bb3f252bc498b4f453392e638f719b01275ec
Manab Bera <manabbera2005@gmail.com>
2026-04-10 21:16:24 +0530

    69348

--- orders/views.py
+++ orders/views.py
@@ -1,19 +1,8 @@
-from rest_framework import generics
+from rest_framework.generic import RetriveAPIView
 from rest_framework.permission import IsAuthenticated
 from .models import Order
 from .serializers import OrderSreializer
 
-class OrderHistoryListView(generics.ListAPIView):
+class OrderHistoryListView(RetriveAPIView):
+    queryset=Order.objects.all()
     serializer_class=OrderSreializer
-    permission_classes=[IsAuthenticated]
-
-    def get_queryset(self):
-        return Order.objects.filter(user=self.request.user).order_by('-created_at')
-
-    def resturant_status_view(request):
-        open_time,close_time=get_todat_operating_hours()
-
-        if open_time and close_time:
-            status=f"We are open today from {open_time} to {close_time}!"
-        else:
-            status="Sory, we are closed today."
