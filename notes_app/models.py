77460a4242c61ab5b7ef6bb2469aa54367f539c7
Manab Bera <manabbera2005@gmail.com>
2026-04-13 14:08:36 +0530

    69678

--- notes_app/models.py
+++ notes_app/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
-class Category(models.Model):
-    name=models.CharField(max_length=255,unique=True)
-    def __str__(self):
-        return self.name
-class MenuItem(models.Model):
-    title=models.CharField(max_length=255)
-    price=models.DecimalField(max_digits=6,decimal_place=2)
-    category=models.ForeignKey(Category,on_delete=models.PROTECT)
+class LoyaltyProgram(models.Model):
+    name-models.CharField(max_length=50,unique=True,help_text="The name of the loyalty tier (e.g., 'Silver Member')")
+    points_required=models.PositiveIntegerField(unique=True,help_text="Maximum points required to reach this tier")
+    discount_percentage=models.DecimalField(max_digits=5,decimal_place=2,validators=[MinValueValidator(0),MaxValueValidator(100)],help_text="Percentage discount (e.g., 5.00 for 5%)")
+    description = models.TextField(blank=True,help_text="Brief explanation of the benefitd for this tier")
+    class Meta:
+        ordering=['points_required']
+        verbose_name="Loyalty Program Tier"
 
     def __str__(self):
-        return self.title
+        return f"{self.name} ({self.points_required} +points)"
