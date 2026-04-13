9910adc526882a3736cd28d5c2265c7a5e0d9b82
Manab Bera <manabbera2005@gmail.com>
2026-04-13 14:25:04 +0530

    69677

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from home.models import Restaurant
 
-# Create your tests here.
+class RestaurantInfoAPiTest(APITestCase):
+    def test_get_restauran_info(self):
+        restaurant=Restaurant.objects.create(name='Test Restaurant', address='123 Test St')
+        url = '/api/restaurant-info'
+        response =self.client.get(url)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        data=response.data[0] if isinstance(response.data,list) else response.data
+        self.assertEqual(data['name'],'Test Restaurant')
+        self.assertEqual(data['address'],'123 Test St')
